8ecdee780d5c4644e868c1220949147a0afe69e2
liukunspring <just_for_rd@163.com>
2025-03-25 09:11:18 +0800

    准备做数据集

---
+++ tokenStar/cut_file.py
@@ -0,0 +1,19 @@
+import os 
+from __init__ import PROJECT_ROOT
+def cut_last_n_file(column_num,file_path,target_file):
+    """
+    切割文件的后n列之后的数据
+    """
+    with open(file_path,'r',encoding='utf-8',errors='ignore') as in_file,open(target_file,'w',encoding='utf-8')as tf:
+        line_data=in_file.readline()
+        while line_data!='':
+            cut_line=line_data[column_num:]
+            tf.write(cut_line)
+            #print(cut_line)
+            line_data=in_file.readline()
+
+if __name__=='__main__':
+    cut_last_n_file(31,
+                    os.path.join(PROJECT_ROOT,'resource','tokendata','applogcat-1.log'),
+                    os.path.join(PROJECT_ROOT,'resource','tokendata','applogcat-1-cut.log')
+                    )
