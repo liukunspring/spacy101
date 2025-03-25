43449b7ab588bb1dd884e644bdf9429df96d853a
liukunspring <just_for_rd@163.com>
2025-03-26 00:13:54 +0800

    注释语料

--- main.py
+++ main.py
@@ -1,3 +1,48 @@
 import spacy
-nlp = spacy.blank("en")
-nlp.tokenizer = my_tokenizer
+from spacy.language import Language
+from spacy.tokens import Doc
+
+import os 
+from tokenStar import tokennizers_spacy
+from config import PROJECT_ROOT
+def build_nlp_instance()->Language:
+    nlp = spacy.blank("en")
+    ner=nlp.add_pipe('ner')
+    ner.add_label("TID")
+    ner.add_label("ORG")
+    ner.add_label("IP_ADDRESS")
+    ner.add_label("LOCATION")
+    ner.add_label("FLOAT_NUMER")
+    ner.add_label("COMMON_NUMER")
+    nlp.initialize()
+    nlp.tokenizer = tokennizers_spacy.BertTokenizer(nlp.vocab,os.path.join(PROJECT_ROOT, "applog-tokenizer.json"))
+    nlp.add_pipe("thread_tid_entities", before="ner")
+    nlp.add_pipe("ip_address_entity",before="thread_tid_entities")
+    nlp.add_pipe("float_number_entity",before="ip_address_entity")
+    nlp.add_pipe("common_number_entity",before="ip_address_entity")
+    return nlp
+
+class Ner:
+    def __init__(self):
+        pass
+def dump_ner(text,nlp:Language):
+    doc=nlp(text)
+    ner_ret={
+        'text':text,
+        'entites':[]
+    }
+    for ent in doc.ents:
+        ent.label_
+        ner_ret['entites'].append(
+            {
+                'entity_type':ent.label_,
+                'start_char':ent.start_char,
+                'end_char':ent.end_char,
+                'value':ent.text
+            }
+        )
+    return ner_ret
+if __name__=='__main__':
+    nlp=build_nlp_instance()
+    text="I eSpaceService: WatchThread {4786} TcpCirChannel.shutdown(TcpCirChannel.java:257) Login_By_Step-> shut down now connec"
+    dump_ner(text,nlp)
--- tokenStar/cut_file.py
+++ tokenStar/cut_file.py
@@ -1,16 +1,28 @@
 import os 
 from __init__ import PROJECT_ROOT
+import re
+number_regex=re.compile('/d')
 def cut_last_n_file(column_num,file_path,target_file):
     """
     切割文件的后n列之后的数据
     """
+    i=0 
     with open(file_path,'r',encoding='utf-8',errors='ignore') as in_file,open(target_file,'w',encoding='utf-8')as tf:
         line_data=in_file.readline()
         while line_data!='':
             cut_line=line_data[column_num:]
-            tf.write(cut_line)
             #print(cut_line)
             line_data=in_file.readline()
+            if number_regex.search(cut_line):
+                tf.write(cut_line)
+                i+=1
+            if i>5000:
+                break
+
+def dump_ner(text):
+    # 转换ner的数据
+    
+    pass
 
 if __name__=='__main__':
     cut_last_n_file(31,
